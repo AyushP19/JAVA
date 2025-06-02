d833a1c154283329b672fed468651521a0ed5b25
AyushP19 <ayushpandaey.ayush7878@gmail.com>
2025-06-03 00:11:46 +0530

    Your message here

--- src/first.java
+++ src/first.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class first {
-    public static void main(String [] args){
-         System.out.println("hello world");
+    public static void main(String[] args) {
+        int i,temp,sum=0;
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+//        int num2= sc.nextInt();
+        for(;num1!=0;)
+        {
+            temp =num1%10;
+            sum=sum*10+temp;
+            num1=num1/10;
+        }
+        System.out.println(sum);
     }
-
 }
